c3ea0108b0eaf2888ecd459fcd9da32a4a20bb7d
William Soukkachang <WSoukkachang@yahoo.com>
2019-03-19 21:51:45 -0500

    added array, completed day 1 challenges

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React from 'react';
 import TodoList from './components/TodoComponents/TodoList';
 import TodoForm from './components/TodoComponents/TodoForm';
 
+const todos = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -11,12 +23,14 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      toDos: todos,
       todo: [{task: '', id: Date.now(), completed: false}],
       task: ''
     }
   }
 
   render() {
+
     return (
       <div>
         <TodoForm />
